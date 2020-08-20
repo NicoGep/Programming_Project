aa9a27c16da3a48104ffe80b1a20877c412a7057
Lukas Werner <61937075+Lukas-Werner@users.noreply.github.com>
2020-08-20 20:57:21 +0200

    added MenuScreen

---
+++ ATdIT-Projekt/src/funktionen/AusloggenFunktion.java
@@ -0,0 +1,14 @@
+package funktionen;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class AusloggenFunktion implements ActionListener {
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
