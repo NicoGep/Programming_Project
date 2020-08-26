b8d9666f868b8865139557c88c3a3aacb425c66b
Lukas Werner <61937075+Lukas-Werner@users.noreply.github.com>
2020-08-26 19:57:02 +0200

    Menu editet

--- ATdIT-Projekt/src/funktionen/AusloggenFunktion.java
+++ ATdIT-Projekt/src/funktionen/AusloggenFunktion.java
@@ -5,6 +5,7 @@ import java.awt.event.ActionListener;
 
 import master.Fenster;
 import screens.LoginScreen;
+import screens.MeinProfil;
 import screens.MenuScreen;
 
 public class AusloggenFunktion implements ActionListener {
@@ -12,7 +13,7 @@ public class AusloggenFunktion implements ActionListener {
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		if (e.getSource() == MenuScreen.ausloggenButton) {
-			Fenster.addToFrame(new LoginScreen());
+			Fenster.addToFrame(new MeinProfil());
 		}
 		
 	}
--- ATdIT-Projekt/src/screens/MenuScreen.java
+++ ATdIT-Projekt/src/screens/MenuScreen.java
@@ -35,21 +35,33 @@ public class MenuScreen extends MasterScreen {
 //		menuPanel.add(menuLabel, java.awt.BorderLayout.CENTER);
 //		this.add(menuPanel);
 		
+		menuPanel = new JPanel();
+		menuPanel.setBounds(75, 50, 300, 50);
+		menuLabel = new JLabel("MENÜ");
+		menuLabel.setBackground(Color.white);
+		menuLabel.setFont(new Font("Calibri", Font.BOLD, 30));
+		menuPanel.add(menuLabel);
+		menuPanel.setBackground(Color.white);
+		
+		
 		karteButton = new JButton("Karte");
 		karteButton.setBackground(Color.white);
-		karteButton.setBounds(75, 100, 300, 50);
+		karteButton.setBounds(75, 150, 300, 50);
 		karteButton.setBorderPainted(false);
 		karteButton.setFont(new Font("Calibri", Font.BOLD, 16));
+		
 		mountainbikeKarteButton = new JButton("Mountainbiking");
 		mountainbikeKarteButton.setBackground(Color.white);
-		mountainbikeKarteButton.setBounds(75, 200, 300, 50);
+		mountainbikeKarteButton.setBounds(75, 250, 300, 50);
 		mountainbikeKarteButton.setBorderPainted(false);
 		mountainbikeKarteButton.setFont(new Font("Calibri", Font.BOLD, 16));
+		
 		meinProfilButton = new JButton("Mein Profil");
-		meinProfilButton.setBounds(75, 300, 300, 50);
+		meinProfilButton.setBounds(75, 350, 300, 50);
 		meinProfilButton.setBackground(Color.white);
 		meinProfilButton.setBorderPainted(false);
 		meinProfilButton.setFont(new Font("Calibri", Font.BOLD, 16));
+		
 		ausloggenButton = new JButton("Ausloggen");
 		ausloggenButton.setBackground(Color.white);
 		ausloggenButton.setBounds(170, 600, 125, 50);
@@ -60,6 +72,7 @@ public class MenuScreen extends MasterScreen {
 		mountainbikeKarteButton.addActionListener(new MountainBikeFunktion());
 		//meinProfilButton.addActionListener(new MeinProfilFunktion());
 		ausloggenButton.addActionListener(new AusloggenFunktion());
+		this.add(menuPanel);
 		this.add(karteButton);
 		this.add(mountainbikeKarteButton);
 		this.add(meinProfilButton);
