9da357935cee9732279a1048d3f9b6ca675f0777
Lukas Werner <61937075+Lukas-Werner@users.noreply.github.com>
2020-09-10 19:40:54 +0200

    Map slider functionality
    + some documentation

--- ATdIT-Projekt/src/funktionen/KarteSliderFunktion.java
+++ ATdIT-Projekt/src/funktionen/KarteSliderFunktion.java
@@ -4,39 +4,30 @@ import javax.swing.JSlider;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 
-import connection.Benutzer;
-import exceptions.InputException;
 import screens.KarteScreen;
 
-public class KarteSliderFunktion extends Thread implements ChangeListener {
+public class KarteSliderFunktion implements ChangeListener {
 	JSlider sliderFunktionHoehe;
 	JSlider sliderFunktionLaenge;
-	int streckenLaenge;
-	int hoehenMeter;
-	int sleeper = 0;
+	protected static int streckenLaenge;
+	protected static int hoehenMeter;
+
 	public KarteSliderFunktion(KarteScreen karteScreen) {
 		sliderFunktionLaenge = karteScreen.streckenLaengeSlider;
 		sliderFunktionHoehe = karteScreen.hoehenunterschiedSlider;
-	}
-	
-	@Override
-	public void stateChanged(ChangeEvent e) {
-		// TODO Auto-generated method stub
-		streckenLaenge = sliderFunktionLaenge.getValue();
-		hoehenMeter = sliderFunktionHoehe.getValue();
-		try {
-			while (sleeper < 1) {
-				try {
-					Thread.sleep(3000);
-					Benutzer.updateParameter(streckenLaenge, hoehenMeter);
-				} catch (InterruptedException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-				}
-			}
-		} catch (InputException e1) {
-			e1.printStackTrace();
-		}
+		KarteUpdateFunktion updateKarte = new KarteUpdateFunktion();
+		updateKarte.start();
 	}
 
+	@Override
+	public void stateChanged(ChangeEvent e) {
+		
+		if (e.getSource() == sliderFunktionLaenge) {
+			streckenLaenge = sliderFunktionLaenge.getValue();
+		}
+		else if(e.getSource() == sliderFunktionHoehe) {
+			hoehenMeter = sliderFunktionHoehe.getValue();
+		}
+		
+	}
 }
