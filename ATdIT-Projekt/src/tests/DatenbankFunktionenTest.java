5f46c10a2dfa54a115c0823db7e5428ea2c75ad1
philfen <philfen@outlook.de>
2020-09-11 17:24:51 +0200

    Update DatenbankFunktionenTest.java

--- ATdIT-Projekt/src/tests/DatenbankFunktionenTest.java
+++ ATdIT-Projekt/src/tests/DatenbankFunktionenTest.java
@@ -23,7 +23,15 @@ public class DatenbankFunktionenTest {
 		try {
 			DatabaseConnection.connectDatabase();
 			if (AdminFunctions.findUser(name) == null) {
-
+				AdminFunctions.addUser(name, password);
+				Benutzer.setEmail(email);
+				Benutzer.setNiveau(niveau);
+				Benutzer.setProfilbild(profilbild);
+			}
+			else {
+				Benutzer.setEmail(email);
+				Benutzer.setNiveau(niveau);
+				Benutzer.setProfilbild(profilbild);
 			}
 			Benutzer.loginUser(name, password);
 			assertEquals(name, Benutzer.getName());
@@ -32,6 +40,99 @@ public class DatenbankFunktionenTest {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (LoginCredentialsException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InputException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+	@Test
+	public void niveauTest() {
+		try {
+			DatabaseConnection.connectDatabase();
+			if (AdminFunctions.findUser(name) == null) {
+				AdminFunctions.addUser(name, password);
+				Benutzer.setEmail(email);
+				Benutzer.setNiveau(niveau);
+				Benutzer.setProfilbild(profilbild);
+			}
+			else {
+				Benutzer.setEmail(email);
+				Benutzer.setNiveau(niveau);
+				Benutzer.setProfilbild(profilbild);
+			}
+			Benutzer.loginUser(name, password);
+			assertEquals(niveau, Benutzer.getNiveau());
+			DatabaseConnection.disconnectDatabase();
+		} catch (DatabaseConnectException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (LoginCredentialsException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InputException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+	@Test
+	public void emailTest() {
+		try {
+			DatabaseConnection.connectDatabase();
+			if (AdminFunctions.findUser(name) == null) {
+				AdminFunctions.addUser(name, password);
+				Benutzer.setEmail(email);
+				Benutzer.setNiveau(niveau);
+				Benutzer.setProfilbild(profilbild);
+			}
+			else {
+				Benutzer.setEmail(email);
+				Benutzer.setNiveau(niveau);
+				Benutzer.setProfilbild(profilbild);
+			}
+			Benutzer.loginUser(name, password);
+			assertEquals(email, Benutzer.getEmail());
+			DatabaseConnection.disconnectDatabase();
+		} catch (DatabaseConnectException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (LoginCredentialsException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InputException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+	@Test
+	public void profilbildTest() {
+		try {
+			DatabaseConnection.connectDatabase();
+			if (AdminFunctions.findUser(name) == null) {
+				AdminFunctions.addUser(name, password);
+				Benutzer.setEmail(email);
+				Benutzer.setNiveau(niveau);
+				Benutzer.setProfilbild(profilbild);
+			}
+			else {
+				Benutzer.setEmail(email);
+				Benutzer.setNiveau(niveau);
+				Benutzer.setProfilbild(profilbild);
+			}
+			Benutzer.loginUser(name, password);
+			assertEquals(profilbild, Benutzer.getProfilBild());
+			DatabaseConnection.disconnectDatabase();
+		} catch (DatabaseConnectException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (LoginCredentialsException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InputException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
