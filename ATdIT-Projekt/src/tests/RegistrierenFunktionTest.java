9be6005b87e6c22ce4726ea3516e146ca074633a
VeroTara <62519927+VeroTara@users.noreply.github.com>
2020-09-11 18:05:11 +0200

    test1

--- ATdIT-Projekt/src/tests/RegistrierenFunktionTest.java
+++ ATdIT-Projekt/src/tests/RegistrierenFunktionTest.java
@@ -1,5 +1,32 @@
 package tests;
 
-public class RegistrierenFunktionTest {
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.Test;
+
+import connection.Benutzer;
+import connection.DatabaseConnection;
+import exceptions.DatabaseConnectException;
+import exceptions.InputException;
+import exceptions.LoginCredentialsException;
+import funktionen.AdminFunctions;
+
+public class RegistrierenFunktionTest {
+	static String name = "Test";
+	static String password = "password";
+	static String email = "email@test.de";
+	
+	@Test
+	public void registrierenTest() {
+		try {
+			DatabaseConnection.connectDatabase();
+			if (AdminFunctions.findUser(name) == null) {
+				AdminFunctions.addUser();
+				
+	
+	
+	
+	
+	
+	
 }
