9469bbabda63c38f19d3a0de58d32beb73f83a6a
philfen <philfen@outlook.de>
2020-10-30 17:26:54 +0100

    Revert "Revert "Internationalization level""
    This reverts commit 5d9dc7d428f24e7c96c9188bf6b563f486025c45.

--- ATdIT-ProjektM/src/main/java/funktionen/MyProfileFunction.java
+++ ATdIT-ProjektM/src/main/java/funktionen/MyProfileFunction.java
@@ -3,6 +3,7 @@ package funktionen;
 import connection.User;
 
 import java.util.List;
+import java.util.ResourceBundle;
 
 import connection.Groups;
 import screens.MyProfile;
@@ -18,7 +19,7 @@ public class MyProfileFunction {
 	public String mail;
 	public String profilepictureaddress;
 	public List<Groups> groupList;
-
+	private final ResourceBundle STRING_TEXT = ResourceBundle.getBundle("i18n/Funktionen/MyProfileFunction/myprofilefunction");
 	/**
 	 * method to load user data
 	 */
@@ -29,6 +30,15 @@ public class MyProfileFunction {
 		MyProfile.mail = b.getEmail();
 		MyProfile.profilepictureaddress = b.getProfilBild();
 		addGroups();
+		if ((b.getNiveau()).equals("1")) {
+			MyProfile.level = STRING_TEXT.getString("beginner");
+		}
+		else if ((b.getNiveau()).equals("2")) {
+			MyProfile.level = STRING_TEXT.getString("medium");
+		}
+		else if ((b.getNiveau()).equals("3")) {
+			MyProfile.level = STRING_TEXT.getString("pro");
+		}
 	}
 
 	/**
