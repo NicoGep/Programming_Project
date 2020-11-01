1b79a7dabe089d62ddb0bce0ab7957ba0b2a27c7
VeroTara <62519927+VeroTara@users.noreply.github.com>
2020-11-01 16:50:28 +0100

    Update RegistrationTest.java

--- ATdIT-ProjektM/src/test/java/RegistrationTest.java
+++ ATdIT-ProjektM/src/test/java/RegistrationTest.java
@@ -1,3 +1,4 @@
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import javax.swing.JLabel;
@@ -8,29 +9,29 @@ import org.junit.jupiter.api.Test;
 
 import connection.User;
 import connection.Validator;
+import exceptions.ChangePasswordException;
 import exceptions.InputException;
 import exceptions.LoginCredentialsException;
+import funktionen.ChangePasswordFunction;
 import funktionen.RegisterFunction;
+import screens.Registration;
 
-public class RegistrationTest  {
-	
+public class RegistrationTest {
+
 	@Test
 	public void testUserAlreadyExists() throws LoginCredentialsException, InputException {
 		Validator.createValidator();
-		JTextField testUser = new JTextField("TestBenutzer1");
+		JTextField testUser = new JTextField("TestBenutzer2");
 		JTextField testMail = new JTextField("test@beispiel.de");
 		JPasswordField testPass = new JPasswordField("pass");
 		JPasswordField testPassAgain = new JPasswordField("pass");
-		
-		RegisterFunction test = new RegisterFunction (testUser, testMail, testPass, testPassAgain);
-		JLabel userAlreadyExists = new JLabel("Benutzer existiert bereits");
-		userAlreadyExists.setVisible(false);
-		
-		test.register();
-		assertTrue(userAlreadyExists.isVisible());
 
-		}
-		
+		RegisterFunction test = new RegisterFunction(testUser, testMail, testPass, testPassAgain);
+		Registration.userexistsLabel = new JLabel();
+		Registration.userexistsLabel.setVisible(false);
+
+		test.register();
+		assertTrue(	Registration.userexistsLabel.isVisible());
 	}
 
-
+}
