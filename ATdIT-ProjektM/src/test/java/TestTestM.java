2b2629ff95c667496166123d18e2c6c863e2a296
philfen <philfen@outlook.de>
2020-11-01 16:52:42 +0100

    Create TestTestM.java

---
+++ ATdIT-ProjektM/src/test/java/TestTestM.java
@@ -0,0 +1,10 @@
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class TestTestM {
+	@Test
+	public void testtest() {
+		assertTrue(true);
+	}
+}
