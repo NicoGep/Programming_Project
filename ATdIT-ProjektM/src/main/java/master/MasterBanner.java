c1070d0f1e151f9f77f9de4bf894b0adf0d4a07e
NicoGep <62658798+NicoGep@users.noreply.github.com>
2020-11-03 16:27:20 +0100

    Name Label in banner

--- ATdIT-ProjektM/src/main/java/master/MasterBanner.java
+++ ATdIT-ProjektM/src/main/java/master/MasterBanner.java
@@ -5,6 +5,7 @@ import java.util.ResourceBundle;
 
 import javax.swing.*;
 
+import connection.User;
 import screens.MenuScreen;
 
 /** Class for the MasterBanner to have uniform screens
@@ -18,6 +19,7 @@ public class MasterBanner extends JPanel {
 	private final int SIZE_X = 450, SIZE_Y = 100;
 	
 	JButton backButton, logoutButton, menuButton;
+	JLabel nameLabel;
 	
 //	JLabel nameLabel;
 	private final ResourceBundle STRING_TEXT;
@@ -36,11 +38,17 @@ public class MasterBanner extends JPanel {
 		
 		backButton = new JButton(STRING_TEXT.getString("back"));
 		logoutButton = new JButton(STRING_TEXT.getString("log_out"));
-		menuButton = new JButton(STRING_TEXT.getString("menu")); 		
+		menuButton = new JButton(STRING_TEXT.getString("menu")); 	
+		
 		
 		this.add(backButton);
 		this.add(logoutButton);
 		this.add(menuButton);
+		
+		if(User.getLoggedUser() != null) {
+			nameLabel = new JLabel(STRING_TEXT.getString("welcome_Name") + " " + User.getLoggedUser().getName());
+			this.add(nameLabel);
+		}
 
 		
 		backButton.addActionListener(a -> {
